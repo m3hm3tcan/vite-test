37c2279fe0ec068729781b197f9e9bad8618c972
m3hm3tcan <m3hm3tcan@gmail.com>
2024-03-15 11:01:58 +0100

    lagacy added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    legacy({
+      // for ie11
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),],
   base: "/vite-test/"
 })
